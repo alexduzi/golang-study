07f8fa54aa3eb8a9975ac35eee06311f41009745
alexduzi <duzihd@gmail.com>
2025-06-11 16:23:58 -0300

    sync value with sync.Mutex example

--- goroutines/main.go
+++ goroutines/main.go
@@ -15,35 +15,60 @@ func main() {
 
 	// time.Sleep(time.Duration(time.Hour.Seconds() * float64(5)))
 
-	var wg sync.WaitGroup
-	wg.Add(3)
-	go callDatabase(&wg)
-	go callApi(&wg)
-	go processInternal(&wg)
-	wg.Wait()
+	// var wg sync.WaitGroup
+	// wg.Add(3)
+	// go callDatabase(&wg)
+	// go callApi(&wg)
+	// go processInternal(&wg)
+	// wg.Wait()
+
+	// i := 0
+	// go ChangeNumber(&i, 5)
+	// go ChangeNumber(&i, 10)
+	// go ChangeNumber(&i, 20)
+
+	// time.Sleep(time.Second * 2)
+	// fmt.Println(i)
+
+	var m sync.Mutex
+	i := 0
+	for x := 0; x < 10000; x++ {
+		go func() {
+			m.Lock()
+			i++
+			m.Unlock()
+		}()
+	}
+
+	time.Sleep(time.Second * 5)
+	fmt.Println(i)
 }
 
 // func showMessage(message string) {
 // 	fmt.Println(message)
 // }
 
-func callDatabase(wg *sync.WaitGroup) {
-	time.Sleep(2 * time.Second)
+// func callDatabase(wg *sync.WaitGroup) {
+// 	time.Sleep(2 * time.Second)
 
-	fmt.Println("Finalizado callDatabase")
-	wg.Done()
-}
+// 	fmt.Println("Finalizado callDatabase")
+// 	wg.Done()
+// }
 
-func callApi(wg *sync.WaitGroup) {
-	time.Sleep(2 * time.Second)
+// func callApi(wg *sync.WaitGroup) {
+// 	time.Sleep(2 * time.Second)
 
-	fmt.Println("Finalizado callApi")
-	wg.Done()
-}
+// 	fmt.Println("Finalizado callApi")
+// 	wg.Done()
+// }
 
-func processInternal(wg *sync.WaitGroup) {
-	time.Sleep(1 * time.Second)
+// func processInternal(wg *sync.WaitGroup) {
+// 	time.Sleep(1 * time.Second)
 
-	fmt.Println("Finalizado processInternal")
-	wg.Done()
+// 	fmt.Println("Finalizado processInternal")
+// 	wg.Done()
+// }
+
+func ChangeNumber(i *int, newNumber int) {
+	*i = newNumber
 }
