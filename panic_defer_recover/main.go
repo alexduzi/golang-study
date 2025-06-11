2e85f54c323264af91c814848c827376f2fe8322
alexduzi <duzihd@gmail.com>
2025-06-10 22:29:51 -0300

    panic defer recover

---
+++ panic_defer_recover/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Inicializando...")
+
+	panicExample()
+	// deferExample()
+
+	fmt.Println("Fim")
+}
+
+func deferExample() {
+	file, err := os.Create("./settings.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	file.Write([]byte("teste"))
+}
+
+func panicExample() {
+	// função anonima
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recover")
+		}
+	}()
+
+	_, err := os.Open("./panic.txt")
+
+	if err != nil {
+		panic(err)
+	}
+}
