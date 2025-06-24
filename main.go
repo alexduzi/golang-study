0af4814c7b781ebd74cb3b6dd66333b56dc34092
Alex Henrique Duzi <duzihd@gmail.com>
2025-06-24 09:53:48 -0300

    Channels example

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan int)
+	exit := make(chan bool)
+
+	go func() {
+		for i := 0; i <= 20; i++ {
+			ch <- i
+		}
+		exit <- true
+	}()
+
+	defer close(ch)
+	defer close(exit)
+
+	for {
+		select {
+		case value := <-ch:
+			fmt.Printf("Receiving: -> %v\n", value)
+		case value := <-exit:
+			fmt.Println("End!", value)
+		default:
+			fmt.Println("no message received")
+		}
+		time.Sleep(time.Second)
+	}
+}
