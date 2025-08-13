f0fc51bc284b5d0bc2a8f55de56fce8e9227f7a2
alexduzi <duzihd@gmail.com>
2025-08-13 20:19:44 -0300

    server mux

---
+++ server-mux/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Test"))
+	})
+	http.ListenAndServe(":8080", mux)
+}
