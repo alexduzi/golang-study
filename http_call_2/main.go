5fc39741a9886ff39b431e83842a3191179f1cfc
alexduzi <duzihd@gmail.com>
2025-08-14 21:43:32 -0300

    http example with timeout

---
+++ http_call_2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	client := http.Client{Timeout: time.Second}
+	resp, err := client.Get("http://google.com")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	print(string(body))
+}
