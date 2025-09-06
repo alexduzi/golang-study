ea9d5751b44a3a00b5931c1d8a5ac5d49a5cc581
alexduzi <duzihd@gmail.com>
2025-09-06 15:17:41 -0300

    working with relations

--- database/2/main.go
+++ database/2/main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-	"fmt"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+type Category struct {
+	ID   int `gorm:"primaryKey"`
+	Name string
+}
+
 type Product struct {
 	gorm.Model
-	ID    uint `gorm:"primaryKey"`
-	Name  string
-	Price float64
+	ID         uint `gorm:"primaryKey"`
+	Name       string
+	Price      float64
+	CategoryID int
+	Category   Category
 }
 
 func main() {
+	// SimpleGormOperations()
+
 	dsn := "root:root@tcp(localhost:3306)/goexpertgorm?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -22,76 +29,17 @@ func main() {
 	}
 
 	// cria tabela se baseando na struct
-	db.AutoMigrate(&Product{})
+	db.AutoMigrate(&Product{}, &Category{})
 
-	// // inserindo
-	// db.Create(&Product{
-	// 	Name:  "Playstation 5",
-	// 	Price: 699.00,
-	// })
+	// create cateogry
+	category := Category{Name: "Eletronics"}
+	db.Create(&category)
 
-	// // inserindo em batch
-	// products := []Product{
-	// 	{Name: "Notebook", Price: 5000.00},
-	// 	{Name: "Smartphone", Price: 2000.00},
-	// 	{Name: "Aoc Monitor 27pol", Price: 1000.00},
-	// }
-	// db.Create(&products)
-
-	// // retornando primeiro produto
-	// var product Product
-	// db.First(&product)
-	// fmt.Printf("%+v \n", product)
-
-	// // retornando o produto de id 2
-	// var product2 Product
-	// db.First(&product2, 2)
-	// fmt.Printf("%+v \n", product2)
-
-	// // retornando primeiro produto com cláusula where
-	// var smartphone Product
-	// db.First(&smartphone, "name = ?", "Smartphone")
-	// fmt.Printf("%+v \n", smartphone)
-
-	// // retornando todos os produtos
-	// var products []Product
-	// db.Find(&products)
-	// for _, prod := range products {
-	// 	fmt.Printf("%+v \n", prod)
-	// }
-
-	// // retornando com paginação
-	// var paginatedList []Product
-	// db.Limit(2).Offset(2).Find(&paginatedList)
-	// for _, prod := range paginatedList {
-	// 	fmt.Printf("%+v \n", prod)
-	// }
-
-	// // // retornando com where
-	// var products []Product
-	// db.Where("price > ?", 1000).Find(&products)
-	// for _, prod := range products {
-	// 	fmt.Printf("%+v \n", prod)
-	// }
-
-	// // // retornando com where
-	// var products []Product
-	// db.Where("name like ?", "%book%").Find(&products)
-	// for _, prod := range products {
-	// 	fmt.Printf("%+v \n", prod)
-	// }
-
-	// // atualizando o primeiro registro
-	// var p Product
-	// db.First(&p)
-	// p.Name = "New Mouse"
-	// db.Save(&p)
-
-	var p2 Product
-	db.First(&p2, 1)
-	fmt.Println(p2.Name)
-
-	// deletando registro
-	db.Delete(&p2)
-
+	// create product
+	product := Product{
+		Name:       "Notebook",
+		Price:      1000.00,
+		CategoryID: category.ID,
+	}
+	db.Create(&product)
 }
