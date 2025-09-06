5f7a965aeb763f171d54c100943059cf02ba45eb
alexduzi <duzihd@gmail.com>
2025-09-06 15:23:09 -0300

    fetching relations with db.Preload

--- database/2/main.go
+++ database/2/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -28,18 +30,27 @@ func main() {
 		panic(err)
 	}
 
-	// cria tabela se baseando na struct
-	db.AutoMigrate(&Product{}, &Category{})
+	// // cria tabela se baseando na struct
+	// db.AutoMigrate(&Product{}, &Category{})
 
-	// create cateogry
-	category := Category{Name: "Eletronics"}
-	db.Create(&category)
+	// // create cateogry
+	// category := Category{Name: "Eletronics"}
+	// db.Create(&category)
 
-	// create product
-	product := Product{
-		Name:       "Notebook",
-		Price:      1000.00,
-		CategoryID: category.ID,
+	// // create product
+	// product := Product{
+	// 	Name:       "Notebook",
+	// 	Price:      1000.00,
+	// 	CategoryID: category.ID,
+	// }
+	// db.Create(&product)
+
+	// carregando as categorias com Preload
+	var products []Product
+	db.Preload("Category").Find(&products)
+	for _, prod := range products {
+		fmt.Printf("%+v \n", prod)
+		fmt.Println()
+		fmt.Printf("%+v \n", prod.Category)
 	}
-	db.Create(&product)
 }
