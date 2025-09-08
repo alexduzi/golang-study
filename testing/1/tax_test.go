7f9a005fb6dba51dd3f7d90dae7b94292425a107
alexduzi <duzihd@gmail.com>
2025-09-08 20:37:22 -0300

    fuzz example

--- testing/1/tax_test.go
+++ testing/1/tax_test.go
@@ -39,3 +39,16 @@ func BenchmarkCalculateTax(b *testing.B) {
 		CalculateTax2(500.0)
 	}
 }
+
+func FuzzCalculateTax(f *testing.F) {
+	seed := []float64{-1, -2, -2.5, 500.0, 1000.0, 1500.0}
+	for _, amount := range seed {
+		f.Add(amount)
+	}
+	f.Fuzz(func(t *testing.T, amount float64) {
+		result := CalculateTax(amount)
+		if amount <= 0 && result != 0 {
+			t.Errorf("Received %f but expected 0", result)
+		}
+	})
+}
