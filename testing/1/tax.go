3c3d0c5f03dcdfe3cd9d5ed4eb6c24d1dab0e14f
alexduzi <duzihd@gmail.com>
2025-09-08 20:12:29 -0300

    benchmark tests

--- testing/1/tax.go
+++ testing/1/tax.go
@@ -1,6 +1,16 @@
 package tax
 
+import "time"
+
 func CalculateTax(amout float64) float64 {
+	if amout >= 1000 {
+		return 10.0
+	}
+	return 5.0
+}
+
+func CalculateTax2(amout float64) float64 {
+	time.Sleep(time.Millisecond)
 	if amout >= 1000 {
 		return 10.0
 	}
--- testing/1/tax_test.go
+++ testing/1/tax_test.go
@@ -33,3 +33,9 @@ func TestCalculateTaxBatch(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkCalculateTax(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax2(500.0)
+	}
+}
