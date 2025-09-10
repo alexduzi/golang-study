fe557952d9b42e2ea363be63c6df83367544eda4
alexduzi <duzihd@gmail.com>
2025-09-09 21:06:23 -0300

    testing new user

---
+++ apis/internal/entity/user_test.go
@@ -0,0 +1,18 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUser(t *testing.T) {
+	user, err := NewUser("Alex Duzi", "email@gmail.com", "123456")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.NotEmpty(t, user.ID)
+	assert.NotEmpty(t, user.Password)
+	assert.Equal(t, "Alex Duzi", user.Name)
+	assert.Equal(t, "email@gmail.com", user.Email)
+}
