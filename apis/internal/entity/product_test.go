888262041268a8eba40740803da66896fbc3031a
alexduzi <duzihd@gmail.com>
2025-09-10 22:33:58 -0300

    refactor price to float64 and product findall test

--- apis/internal/entity/product_test.go
+++ apis/internal/entity/product_test.go
@@ -7,37 +7,37 @@ import (
 )
 
 func TestNewProduct(t *testing.T) {
-	p, err := NewProduct("Product 1", 10)
+	p, err := NewProduct("Product 1", 10.0)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.NotEmpty(t, p.ID)
 	assert.Equal(t, "Product 1", p.Name)
-	assert.Equal(t, 10, p.Price)
+	assert.Equal(t, 10.0, p.Price)
 }
 
 func TestProductWhenNameIsRequired(t *testing.T) {
-	p, err := NewProduct("", 10)
+	p, err := NewProduct("", 10.0)
 	assert.NotNil(t, err)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrNameIsRequired, err)
 }
 
 func TestProductWhenPriceIsRequired(t *testing.T) {
-	p, err := NewProduct("Product 1", 0)
+	p, err := NewProduct("Product 1", 0.0)
 	assert.NotNil(t, err)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrPriceIsRequired, err)
 }
 
 func TestProductWhenPriceIsInvalid(t *testing.T) {
-	p, err := NewProduct("Product 1", -10)
+	p, err := NewProduct("Product 1", -10.0)
 	assert.NotNil(t, err)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrInvalidPrice, err)
 }
 
 func TestProductValidate(t *testing.T) {
-	p, err := NewProduct("Product 1", 5)
+	p, err := NewProduct("Product 1", 5.0)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.Nil(t, p.Validate())
--- apis/internal/infra/database/product_db_test.go
+++ apis/internal/infra/database/product_db_test.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"fmt"
+	"math/rand/v2"
 	"testing"
 
 	"github.com/alexduzi/golang-study/apis/internal/entity"
@@ -16,7 +18,6 @@ func TestCreateNewProduct(t *testing.T) {
 	}
 	db.AutoMigrate(&entity.Product{})
 
-	db.Where("name = ?", "ProductTest").Delete(&entity.Product{})
 	product, err := entity.NewProduct("ProductTest", 10.00)
 	assert.Nil(t, err)
 	productDB := NewProduct(db)
@@ -31,4 +32,63 @@ func TestCreateNewProduct(t *testing.T) {
 	assert.Equal(t, product.ID, productFound.ID)
 	assert.Equal(t, product.Name, productFound.Name)
 	assert.Equal(t, product.Price, productFound.Price)
+
+	db.Where("name = ?", "ProductTest").Delete(&entity.Product{})
+}
+
+func TestFindProductByID(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file:memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+
+	product, err := entity.NewProduct("ProductTest", 10.00)
+	assert.Nil(t, err)
+	productDB := NewProduct(db)
+
+	err = productDB.Create(product)
+	assert.Nil(t, err)
+
+	productFound, err := productDB.FindByID(product.ID.String())
+	assert.Nil(t, err)
+	assert.Equal(t, product.ID, productFound.ID)
+	assert.Equal(t, product.Name, productFound.Name)
+	assert.Equal(t, product.Price, productFound.Price)
+
+	db.Where("name = ?", "ProductTest").Delete(&entity.Product{})
+}
+
+func TestFindAllProducts(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file:memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.AutoMigrate(&entity.Product{})
+
+	for i := 1; i < 24; i++ {
+		product, err := entity.NewProduct(fmt.Sprintf("ProductTest%d", i), rand.Float64()*100)
+		assert.NoError(t, err)
+		db.Create(product)
+	}
+
+	productDB := NewProduct(db)
+	products, err := productDB.FindAll(1, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, products, 10)
+	assert.Equal(t, "ProductTest1", products[0].Name)
+	assert.Equal(t, "ProductTest10", products[9].Name)
+
+	products, err = productDB.FindAll(2, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, products, 10)
+	assert.Equal(t, "ProductTest11", products[0].Name)
+	assert.Equal(t, "ProductTest20", products[9].Name)
+
+	products, err = productDB.FindAll(3, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, products, 3)
+	assert.Equal(t, "ProductTest21", products[0].Name)
+	assert.Equal(t, "ProductTest23", products[2].Name)
 }
