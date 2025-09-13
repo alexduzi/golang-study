0408afc76d6316ccf4ca1ab578381be97cd443c3
alexduzi <duzihd@gmail.com>
2025-09-13 11:02:30 -0300

    product handler, http.HandleFunc, http file for testing requests

--- apis/cmd/server/main.go
+++ apis/cmd/server/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/alexduzi/golang-study/apis/configs"
+	"github.com/alexduzi/golang-study/apis/internal/entity"
+	"github.com/alexduzi/golang-study/apis/internal/infra/database"
+	"github.com/alexduzi/golang-study/apis/internal/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	cfg, err := configs.LoadConfig(".")
+	_, err := configs.LoadConfig(".")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v", cfg)
+	// fmt.Printf("%+v", cfg)
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":8000", nil)
 }
