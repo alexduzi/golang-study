192377d33d71dab502cf7e084a57221da1ccdd27
alexduzi <duzihd@gmail.com>
2025-09-13 20:25:34 -0300

    getjwt docs

--- apis/internal/webserver/handlers/user_handlers.go
+++ apis/internal/webserver/handlers/user_handlers.go
@@ -12,7 +12,7 @@ import (
 )
 
 type Error struct {
-	Message string
+	Message string `json:"message"`
 }
 
 type UserHandler struct {
@@ -29,17 +29,32 @@ func NewUserHandler(userDB database.UserInterface, jwt *jwtauth.JWTAuth, jwtExpi
 	}
 }
 
+// GetJWT user godoc
+// @Summary           Get a user JWT
+// @Description       Get a user JWT
+// @Tags              users
+// @Accept            json
+// @Produce           json
+// @Param             request      body     dto.GetJwtInput    true    "user credentials"
+// @Success           201          {object} dto.GetJwtOutput
+// @Failure           404          {object} Error
+// @Failure           500          {object} Error
+// @Router            /users/token [post]
 func (h *UserHandler) GetJwt(w http.ResponseWriter, r *http.Request) {
 	var jwtInput dto.GetJwtInput
 	err := json.NewDecoder(r.Body).Decode(&jwtInput)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		err := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(err)
 		return
 	}
 
 	user, err := h.UserDB.FindByEmail(jwtInput.Email)
 	if err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
+		w.WriteHeader(http.StatusNotFound)
+		err := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(err)
 		return
 	}
 
@@ -53,11 +68,7 @@ func (h *UserHandler) GetJwt(w http.ResponseWriter, r *http.Request) {
 		"exp": time.Now().Add(time.Second * time.Duration(h.JwtExpiresIn)).Unix(),
 	})
 
-	accessToken := struct {
-		AccessToken string `json:"access_token"`
-	}{
-		AccessToken: tokenString,
-	}
+	accessToken := dto.GetJwtOutput{AccessToken: tokenString}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
