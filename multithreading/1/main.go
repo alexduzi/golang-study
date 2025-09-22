37be2b265cc015f144b8139e9a8b6f14f3309941
alexduzi <duzihd@gmail.com>
2025-09-22 20:09:58 -0300

    intro to wait group

--- multithreading/1/main.go
+++ multithreading/1/main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	go task("A")
-	go task("B")
+	wg := sync.WaitGroup{}
 
-	time.Sleep(time.Second * 25)
+	wg.Add(20)
+
+	go task("A", &wg)
+
+	go task("B", &wg)
+
+	wg.Wait()
 }
 
-func task(name string) {
+func task(name string, wg *sync.WaitGroup) {
+
 	for i := 0; i < 10; i++ {
 		fmt.Printf("%d: Task %s is running\n", i, name)
 		time.Sleep(time.Second + 1)
+		wg.Done()
 	}
 }
