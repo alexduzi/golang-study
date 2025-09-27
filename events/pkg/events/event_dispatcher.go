905d47329414e692382da48c7f78557adb53eaa4
alexduzi <duzihd@gmail.com>
2025-09-27 11:56:40 -0300

    TestEventDispatcher_Remove

--- events/pkg/events/event_dispatcher.go
+++ events/pkg/events/event_dispatcher.go
@@ -33,6 +33,14 @@ func (ed *EventDispatcher) Dispatch(event EventInterface) error {
 }
 
 func (ed *EventDispatcher) Remove(eventName string, handler EventHandlerInterface) error {
+	if _, ok := ed.handlers[eventName]; ok {
+		for i, h := range ed.handlers[eventName] {
+			if h == handler {
+				ed.handlers[eventName] = append(ed.handlers[eventName][:i], ed.handlers[eventName][i+1:]...)
+				return nil
+			}
+		}
+	}
 	return nil
 }
 
