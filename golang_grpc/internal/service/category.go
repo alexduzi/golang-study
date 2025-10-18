59eaf1302c85a9423a1212f09a08a992a68789a2
Alex Duzi <duzihd@gmail.com>
2025-10-18 15:12:15 -0300

    working with streams implementing CreateCategoryStream

--- golang_grpc/internal/service/category.go
+++ golang_grpc/internal/service/category.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/alexduzi/golang-study/golanggrpc/internal/database"
 	"github.com/alexduzi/golang-study/golanggrpc/internal/pb"
+	"google.golang.org/grpc"
 )
 
 type CategoryService struct {
@@ -64,4 +66,28 @@ func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetReq
 		Name:        category.Name,
 		Description: category.Description,
 	}, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream grpc.ClientStreamingServer[pb.CreateCategoryRequest, pb.CategoryList]) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+		if err != nil {
+			return err
+		}
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		})
+	}
 }
