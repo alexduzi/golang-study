a5dbde74bb4756de1438b27f38f4b1ed21d5acf4
Alex Duzi <duzihd@gmail.com>
2025-10-18 12:04:05 -0300

    exploring grpc world

---
+++ golang_grpc/internal/service/category.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"context"
+
+	"github.com/alexduzi/golang-study/golanggrpc/internal/database"
+	"github.com/alexduzi/golang-study/golanggrpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.CategoryResponse{
+		Category: &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		},
+	}
+
+	return categoryResponse, nil
+}
