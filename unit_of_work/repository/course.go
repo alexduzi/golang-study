debca5a61186d9157b311865b2c4b6e037c43176
Alex Duzi <duzihd@gmail.com>
2025-11-08 18:55:35 -0300

    some refactorings, adding repositories

---
+++ unit_of_work/repository/course.go
@@ -0,0 +1,32 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/alexduzi/unitofwork/entity"
+	"github.com/alexduzi/unitofwork/internal/db"
+)
+
+type CourseRepositoryInterface interface {
+	Insert(ctx context.Context, course entity.Course) error
+}
+
+type CourseRepository struct {
+	DB      *sql.DB
+	Queries *db.Queries
+}
+
+func NewCourseRepository(dtb *sql.DB) *CourseRepository {
+	return &CourseRepository{
+		DB:      dtb,
+		Queries: db.New(dtb),
+	}
+}
+
+func (r *CourseRepository) Insert(ctx context.Context, course entity.Course) error {
+	return r.Queries.CreateCourse(ctx, db.CreateCourseParams{
+		Name:       course.Name,
+		CategoryID: int32(course.CategoryID),
+	})
+}
--- unit_of_work/sql/queries/query.sql
+++ unit_of_work/sql/queries/query.sql
@@ -1,19 +1,5 @@
--- name: ListCategories :many
-SELECT * FROM categories;
-
--- name: GetCategory :one
-SELECT * FROM categories
-WHERE id = ?;
-
--- name: CreateCategory :execresult
-INSERT INTO categories (name)
-VALUES (?);
+-- name: CreateCategory :exec
+INSERT INTO categories (id, name) VALUES (?, ?);
 
 -- name: CreateCourse :exec
-INSERT INTO courses (name, description, category_id)
-VALUES (?, ?, ?);
-
--- name: ListCourses :many
-SELECT c.*, ca.name as category_name
-FROM courses c
-JOIN categories ca ON ca.ID = c.category_id;
+INSERT INTO courses (id, name, category_id) VALUES (?, ?, ?);
