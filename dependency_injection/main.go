b79b315fe32e42cd4831c4a84f893846de5fad24
Alex Duzi <duzihd@gmail.com>
2025-11-09 19:27:12 -0300

    google wire

--- dependency_injection/main.go
+++ dependency_injection/main.go
@@ -7,6 +7,10 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// go install github.com/google/wire/cmd/wire@latest
+// create a wire.go where this file will holds all the mapping config interface x concrete implementation
+// execute wire
+
 func main() {
 	db, err := sql.Open("sqlite3", "./test.db")
 	if err != nil {
