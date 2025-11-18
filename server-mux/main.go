8bf507afd6c3cbc5fc39719b1a7a9c1450c16451
Alex Duzi <duzihd@gmail.com>
2025-11-17 21:37:05 -0300

    NewServeMux

--- server-mux/main.go
+++ server-mux/main.go
@@ -4,8 +4,22 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /books/{id}", GetBookHandler)
+	mux.HandleFunc("GET /books/dir/{d...}", GetBookHandler2)
+
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Test"))
 	})
 	http.ListenAndServe(":8080", mux)
+}
+
+func GetBookHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	w.Write([]byte("Test " + id))
+}
+
+func GetBookHandler2(w http.ResponseWriter, r *http.Request) {
+	d := r.PathValue("d")
+	w.Write([]byte("Test " + d))
 }
